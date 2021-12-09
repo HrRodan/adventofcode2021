f2b5ee8079a1e3d29b7b19daff7de2fa6c154e9b
HrRodan <m.mueller89@gmail.com>
2021-12-09 23:37:13 +0100

    day9 improvement

--- day9/day9.py
+++ day9/day9.py
@@ -9,20 +9,24 @@ import numpy as np
 #
 # ground = np.array([[int(value) for value in linestrip] for line in a.split('\n') if (linestrip := line.strip())])
 
+#alternatives by scipy.ndimage measurement and generic filter
+
 with open('input.txt', 'r') as file:
     ground = np.array([[int(value) for value in linestrip] for line in file if (linestrip := line.strip())])
-
+#basicall 2nd derivative in each direction
 h_2nd = np.diff(np.sign(np.diff(ground, axis=1, prepend=99, append=99)), axis=1)
 v_2nd = np.diff(np.sign(np.diff(ground, axis=0, prepend=99, append=99)), axis=0)
 
 mins = np.where((h_2nd == 2) & (v_2nd == 2), ground, np.nan)
-sum = np.nansum(mins + 1)
-# print(sum)
+print(f'Part1: {np.nansum(mins + 1)}')
 
+#convert to binary
 ground9 = np.where(ground == 9, 1, 0).astype(np.ubyte)
-ground9 = np.pad(ground9, (1, 1), 'constant', constant_values=(1, 1))
 
+#transform to list of indices for easier looping
+#only add non boarders to list
 search_complete = np.transpose((ground9 == 0).nonzero())
+#Use sets for easy tracking
 search_complete_set = {(x, y) for x, y in search_complete}
 
 bases = {}
@@ -34,14 +38,11 @@ for x, y in search_complete_set:
         while local_search:
             x2, y2 = local_search.pop()
             searched_items.add((x2, y2))
-            for i in [-1, 1]:
-                if (x2 + i, y2) in search_complete_set and (x2 + i, y2) not in searched_items:
-                    local_search.add((x2 + i, y2))
-                if (x2, y2 + i) in search_complete_set and (x2, y2 + i) not in searched_items:
-                    local_search.add((x2, y2 + i))
+            for x3, y3 in [(x2 - 1, y2), (x2 + 1, y2), (x2, y2 + 1), (x2, y2 - 1)]:
+                if (x3, y3) in search_complete_set and (x3, y3) not in searched_items:
+                    local_search.add((x3, y3))
             if (x2, y2) in search_complete_set:
                 bases[(x, y)].append((x2, y2))
 
-count = np.array([len(value) for _, value in bases.items()])
-count_max = np.sort(count)[-1:-4:-1]
-print(np.prod(count_max))
+count_max = np.sort([len(value) for _, value in bases.items()])[-1:-4:-1]
+print(f'Part2: {np.prod(count_max)}')
